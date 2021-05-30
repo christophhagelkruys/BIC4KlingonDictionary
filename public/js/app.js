8c7c48a3ccd94305e44e94b3378ed35ecb93dfe8
Christoph Hagelkruys <christoph.hagelkruys@alanda.io>
2021-05-30 17:07:39 +0200

    basic setup

--- public/js/app.js
+++ public/js/app.js
@@ -32371,8 +32371,8 @@ var Form = /*#__PURE__*/function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\devel\Desktop\bic4klingondictionary\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\devel\Desktop\bic4klingondictionary\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/developer/PhpstormProjects/BIC4KlingonDictionary/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/developer/PhpstormProjects/BIC4KlingonDictionary/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
